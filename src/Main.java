b4d5460226504f5df91ea4baf650aa885d0a9301
Nicolas Fofonka <nicolasfofonka08@gmail.com>
2026-01-14 17:49:10 -0300

    mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 import java.util.Scanner;
 
 
@@ -6,7 +10,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-     
+     Curso curso = new Curso();
+     curso.setTitulo("curso java");
+     curso.setDescricao("descricao curso java");
+     curso.setCargaHoraria(8);
+
+     Curso curso2 = new Curso();
+     curso2.setTitulo("curso js");
+     curso2.setDescricao("descricao curso js");
+     curso2.setCargaHoraria(4);
+
+
+     Mentoria mentoria = new Mentoria();
+     mentoria.setTitulo("mentoria de java");
+     mentoria.setDescricao("descricao da mentoria de java");
+     mentoria.setDataprivate(LocalDate.now());
+     System.out.println(curso);
+     System.out.println(curso2);
+     System.out.println(mentoria);
 }
 
 }
---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,42 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate dataprivate ;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataprivate() {
+        return dataprivate;
+    }
+
+    public void setDataprivate(LocalDate dataprivate) {
+        this.dataprivate = dataprivate;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataprivate=" + dataprivate +
+                '}';
+    }
+}
